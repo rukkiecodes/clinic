7279218474b0eda9b66e5563590a4fd22e8b6ff7
rukkiecodes <rukkiecodes@gmail.com>
2026-09-18 06:48:24 +0100

    add contact

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach(button => { 
+  button.addEventListener('click', () => { 
+    window.open("https://wa.me/2349038449588");
+  })
+})
